1bdf147a1c09bcf7583bafc4791474a21267ddf8
Diya <bajoriadiya@gmail.com>
2026-08-18 01:42:52 +0530

    Time: 0 ms (100%), Space: 46.3 MB (81.86%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,19 +1,27 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int ar1[]=new int[nums.length/2];
-        int ar2[]=new int[nums.length/2];
-        for(int i=0;i<nums.length/2;i++)
+        int ar[]=new int[n*2];
+        int j=0;
+        for(int i=0;i<n;i++)
         {
-            ar1[i]=nums[i];
-            ar2[i]=nums[i+n];
+            ar[j++]=nums[i];
+            ar[j++]=nums[i+n];
         }
-        int ar3[]=new int[nums.length];
-        int k=0;
-        for(int i=0;i<nums.length/2;i++)
-        {
-            ar3[k++]=ar1[i];
-            ar3[k++]=ar2[i];
-        }
-        return ar3;
+        return ar;
+        // int ar1[]=new int[nums.length/2];
+        // int ar2[]=new int[nums.length/2];
+        // for(int i=0;i<nums.length/2;i++)
+        // {
+        //     ar1[i]=nums[i];
+        //     ar2[i]=nums[i+n];
+        // }
+        // int ar3[]=new int[nums.length];
+        // int k=0;
+        // for(int i=0;i<nums.length/2;i++)
+        // {
+        //     ar3[k++]=ar1[i];
+        //     ar3[k++]=ar2[i];
+        // }
+        // return ar3;
     }
 }
