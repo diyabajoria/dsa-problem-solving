6f7048b8ffd0ea01f59cef6e616422b537184694
Diya <bajoriadiya@gmail.com>
2026-09-22 11:56:58 +0530

    Time: 0 ms (100%), Space: 48.5 MB (13.99%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,11 +1,10 @@
 class Solution {
     public int search(int[] nums, int target) {
-        boolean present=false;
         int l=0;
         int u=nums.length-1;
         while(l<=u)
         {
-            int m=(l+u)/2;
+            int m=((l+u))/2;
             if(nums[m]==target)
             {
                 return m;
